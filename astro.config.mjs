39ea5a46a15418073c1081100b03ee10ce837a4e
maxcini <maxini@umich.edu>
2024-12-20 15:52:04 -0500

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://maxcini.github.io",
-  base: "",
+  site: "https://maxcini.github.io/",
+  base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
